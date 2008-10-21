c4214eeb95089f85a05058477afa694be73cdd0d
Theresa Kocher <the.kocher@gmx.de>
2008-10-21 15:37:50 +0200

    Add following files: 'myLut.cpp'

---
+++ 2.Uebung/alt/myLut.cpp
@@ -0,0 +1,38 @@
+#include "mex.h"
+
+/*=========================================================================
+ *  Compiling und Linking
+ *  Use     mex -setup  to select a C++ compiler, then tpye:
+ *          mex filename.cpp
+ *=======================================================================*/
+
+/*=========================================================================
+ *  !Memory Consideration For Class Destructors!
+ *  Do not use the mxFree or mxDestroyArray functions in a C++ destructor
+ *  of a class used in a MEX-function. MATLAB performs cleanup of MEX-file
+ *  variables if an error is thrown from the MEX-function, as described in 
+ *  AUTOMATIC CLEANUP OF TEMPORARY ARRAYS.
+ *=======================================================================*/
+
+/*=========================================================================
+ *  Entry point to C MEX-file 
+ *=========================================================================
+ *  prhs: An array of right-hand input arguments
+ *  plhs: An array of left-hand output arguments
+ *  nrhs: The number of right-hand arguments, or the size of the prhs array
+ *  nlhs: The number of left-hand arguments, or the size of the plhs array
+ *=======================================================================*/
+void mexFunction( int nlhs, mxArray *plhs[],
+    int nrhs, const mxArray *prhs[]) 
+{    
+    // check paramters: nlhs, nrhs
+    
+    // check input: uint8
+    
+    // input / output
+   
+    // create output array (mxUINT8_CLASS)
+     
+    // Lut operation
+ 
+}
