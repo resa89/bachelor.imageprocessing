ec42bc8afa52a5201d2eef4ab469e9688103aaee
Theresa Kocher <the.kocher@gmx.de>
2011-03-20 22:41:46 +0100

    Add following files: 'EntryPoint.m'

---
+++ 1.Uebung/alt/EntryPoint.m
@@ -0,0 +1,21 @@
+% -------------------------------------------------------------------------
+% Syntax: 
+%   
+%   imOut = EntryPoint(imIn)
+%
+%   Description: 
+%       Entrypoint reprocess the input image. 
+%   
+%   Entrypoint(imIn) takes,    
+%       imIn          input image (Eingangsbild), uint8
+%    and returns,
+%       imOut         output image (Ausgangsbild), uint8
+%
+%   Example:
+%       imOut = EntryPoint('bild1.jpg');    
+% -------------------------------------------------------------------------
+%
+%   Übungsaufgabennummer, Nachname, Vorname, Matrikelnummer,
+%   Semester, Email-Adresse und Datum
+%
+% -------------------------------------------------------------------------
