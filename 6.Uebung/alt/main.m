934c39c6ae6f01adc1130efbb58aa36b6ac39f87
Theresa Kocher <the.kocher@gmx.de>
2011-04-26 03:43:40 +0200

    Add following files: 'main.m'

---
+++ 6.Uebung/alt/main.m
@@ -0,0 +1,63 @@
+function main()
+    close all;
+    
+    % im = imread('cactus.jpg'); % canny
+    im = imread('bild1.jpg'); % zerocross
+    [m n d] = size(im);
+    
+    if (d == 3)
+        im = color2gray(im);
+    end
+    
+    angle_steps = 256;
+    radius_steps = 256;
+    % im_edge = edge(im, 'canny', [0.1 0.2], 3); % extract edges
+    im_edge = edge(im, 'zerocross', [4 -1], 2); % roberts, log, zerocross
+
+    out = mxLinearHT(im_edge, angle_steps, radius_steps, 0);    
+    max_out = max(max(out));
+    out = double(out) ./ double(max_out);
+    out = out * 255;
+
+    out = 255 - out;        % invert image
+    out = im2double(out);
+    
+    showImage(im, im_edge, out, d);
+    
+    figure;
+    imagesc(out);
+    colormap(gray);
+end
+
+function showImage(im, im_edge, reIm, d)
+    figure;
+    subplot(2,2,1);
+    imagesc(uint8(im));
+    axis image, title Original
+
+    subplot(2,2,2);
+    imagesc(uint8(im_edge));
+    axis image, title 'Binary edge image'
+
+    subplot(2,2,3);
+    imagesc(uint8(reIm));
+    axis image, title 'Hough-Transformation'
+        
+    colormap(gray);
+end
+
+function out = color2gray(im)
+    [m n] = size(im);    
+    out(1:m, 1:n) = (im(1:m,1:n, 1) + im(1:m,1:n, 2) + im(1:m,1:n, 3)) / 3;    
+%     for i = 1:m
+%         for j = 1:n
+%             out(i,j) = (im(i,j, 1) + im(i,j, 2) + im(i,j, 3)) / 3;
+%         end    
+end
+
+
+
+
+
+
+
